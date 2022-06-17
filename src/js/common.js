bc5e0f791d756146ad5ba31ae575776739e49310
alpscloud <winde100kg@gmail.com>
2022-06-17 20:02:03 +0300

    added a few sections

--- src/js/common.js
+++ src/js/common.js
@@ -15,7 +15,62 @@ $(document).ready(function() {
 		timer = setTimeout(function() {
 			body.removeClass('disable-hover');
 		}, 200);
+
+
+		if ($(this).scrollTop() > 0) {
+			$('.header').addClass('is-fixed');
+		} else {
+			$('.header').removeClass('is-fixed');
+		}
 	});
+
+
+
+	// Sliders
+	if ($('.js-promo-slider').length > 0) {
+		$('.js-promo-slider').slick({
+			infinite: true,
+			fade: true,
+			cssEase: 'linear',
+			speed: 1000,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			prevArrow: $('.js-promo-slider-btn-prev'),
+			nextArrow: $('.js-promo-slider-btn-next')
+
+		});
+	}
+
+
+	if ($('.js-simple-slider').length > 0) {
+
+		$('.js-simple-slider').each(function() {
+			var self = $(this);
+			var pagination = self.parents('.simple-slider__wrapper').find('.js-simple-slider-pagination');
+
+			self.slick({
+				infinite: true,
+				speed: 600,
+				arrows: false,
+				dots: true,
+				appendDots: pagination
+			});
+			
+		});
+	}
+
+	if ($('.js-portfolio-slider').length > 0) {
+		$('.js-portfolio-slider').slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 2,
+			speed: 600,
+			arrows: false,
+			dots: true,
+			appendDots: $('.js-portfolio-slider-pagination')
+
+		});
+	}
 
 
 });
